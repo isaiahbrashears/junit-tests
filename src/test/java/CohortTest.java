d2d5728b3a7eeceae039350273f08f29c8b46177
Isaiah Brashears <isaiah.brashears13@gmail.com>
2019-10-08 14:11:06 -0500

    tested part two

--- src/test/java/CohortTest.java
+++ src/test/java/CohortTest.java
@@ -6,10 +6,21 @@ public class CohortTest {
     public void checkCohort(){
         Cohort cohort = new Cohort();
         Student student = new Student(1, "Greg");
+        student.addGrade(90);
+        student.addGrade(60);
+        student.addGrade(80);
+        Student student1 = new Student(2, "Beth");
+        student1.addGrade(50);
+        student1.addGrade(70);
+        student1.addGrade(60);
 
         cohort.addStudent(student);
+        cohort.addStudent(student1);
 
         assertEquals("Greg", cohort.getStudents().get(0).getName());
+        assertEquals(68.33, cohort.getCohortAverage(),2);
+
+
 
 
 
